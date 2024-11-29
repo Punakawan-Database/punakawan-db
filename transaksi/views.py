7ef8f84385aebe968fa74787980b967d4a80edeb
Wisnu Nugroho <wisnunugroho614@gmail.com>
2024-11-29 22:59:40 +0700

    feat: fetch data from db mypay

--- transaksi/views.py
+++ transaksi/views.py
@@ -1,97 +1,84 @@
+from django.http import Http404
 from django.shortcuts import render
+
+from utils import db
+
+logged_user = {
+    "id": "65ae2c07-9011-4a53-bd88-73a6b577691d",
+    "role": "pekerja",
+    "nama": "Jhon Doe",
+}
+
+# logged_user = {
+#     "id": "fasdfasdfa-9011-4a53-bd88-73a6b577691d",
+#     "role": "pengguna",
+#     "nama": "Jono Doe",
+# }
+
+# context = {
+#     "user": {
+#         "role": "pengguna",
+#         "nama": "John Doe",
+#         "no_hp": "085172239073",
+#         "saldo": 1000000000,
+#     },
+#     "transaksi": [
+#         {
+#             "nominal": 10000,
+#             "tanggal": "19-10-1969",
+#             "kategori": "TopUp MyPay",
+#         }
+#     ],
+# }
 
 
 def mypay(request):
+    curr_user = logged_user
+    # curr_user = request.session.get("user")
+
+    user_result = db.query_one(
+        """
+        SELECT
+            p.nama AS nama,
+            p.nohp AS no_hp,
+            p.saldomypay AS saldo
+        FROM pengguna p
+        WHERE p.id = %s
+        """,
+        curr_user["id"],
+    )
+
+    if user_result is None:
+        raise Http404("User not found")
+
+    transactions_result = db.query_all(
+        """
+        SELECT
+            tr.tgl AS tanggal,
+            tr.nominal AS nominal,
+            ktr.nama AS kategori,
+            CASE
+                WHEN ktr.nama IN ('Topup', 'Honor Jasa') THEN TRUE
+                ELSE FALSE
+            END AS is_pemasukan
+        FROM TR_MYPAY tr
+            JOIN KATEGORI_TR_MYPAY ktr ON ktr.id = tr.kategoriid
+        WHERE tr.userid = %s
+        ORDER BY tr.tgl DESC
+        """,
+        curr_user["id"],
+    )
+
     context = {
         "user": {
-            "role": "pengguna",
-            "nama": "John Doe",
-            "no_hp": "085172239073",
-            "saldo": 1000000000,
+            "role": curr_user["role"],
+            "nama": user_result["nama"],
+            "no_hp": user_result["no_hp"],
+            "saldo": user_result["saldo"],
         },
-        "transaksi": [
-            {
-                "nominal": 10000,
-                "tanggal": "19-10-1969",
-                "kategori": "TopUp MyPay",
-            },
-            {
-                "nominal": 20000,
-                "tanggal": "20-10-1969",
-                "kategori": "Pembayaran Transaksi",
-            },
-            {
-                "nominal": 15000,
-                "tanggal": "21-10-1969",
-                "kategori": "Transfer MyPay",
-            },
-            {
-                "nominal": 5000,
-                "tanggal": "22-10-1969",
-                "kategori": "Withdrawal",
-            },
-            {
-                "nominal": 25000,
-                "tanggal": "23-10-1969",
-                "kategori": "TopUp MyPay",
-            },
-            {
-                "nominal": 30000,
-                "tanggal": "24-10-1969",
-                "kategori": "Pembayaran Transaksi",
-            },
-            {
-                "nominal": 12000,
-                "tanggal": "25-10-1969",
-                "kategori": "Transfer MyPay",
-            },
-            {
-                "nominal": 8000,
-                "tanggal": "26-10-1969",
-                "kategori": "Withdrawal",
-            },
-            {
-                "nominal": 18000,
-                "tanggal": "27-10-1969",
-                "kategori": "TopUp MyPay",
-            },
-            {
-                "nominal": 22000,
-                "tanggal": "28-10-1969",
-                "kategori": "Pembayaran Transaksi",
-            },
-            {
-                "nominal": 17000,
-                "tanggal": "29-10-1969",
-                "kategori": "Transfer MyPay",
-            },
-            {
-                "nominal": 9000,
-                "tanggal": "30-10-1969",
-                "kategori": "Withdrawal",
-            },
-            {
-                "nominal": 26000,
-                "tanggal": "31-10-1969",
-                "kategori": "TopUp MyPay",
-            },
-            {
-                "nominal": 14000,
-                "tanggal": "01-11-1969",
-                "kategori": "Pembayaran Transaksi",
-            },
-            {
-                "nominal": 11000,
-                "tanggal": "02-11-1969",
-                "kategori": "Transfer MyPay",
-            },
-            {
-                "nominal": 7000,
-                "tanggal": "03-11-1969",
-                "kategori": "Withdrawal",
-            },
-        ],
+        "transaksi": transactions_result,
     }
+
     return render(request, "mypay.html", context)
 
 
