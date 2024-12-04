313a7b997c5f786f49b52e48aefb7cc9d5e8542c
Wisnu Nugroho <wisnunugroho614@gmail.com>
2024-12-04 22:09:38 +0700

    chore: add trailing slash

--- pekerjaan_jasa/urls.py
+++ pekerjaan_jasa/urls.py
@@ -5,6 +5,6 @@ from pekerjaan_jasa.views import pekerjaan_jasa, pekerjaan_jasa_status
 app_name = "pekerjaan_jasa"
 
 urlpatterns = [
-    path("pekerjaan-jasa", pekerjaan_jasa, name="pekerjaan_jasa"),
-    path("pekerjaan-jasa/status", pekerjaan_jasa_status, name="pekerjaan_jasa_status"),
+    path("pekerjaan-jasa/", pekerjaan_jasa, name="pekerjaan_jasa"),
+    path("pekerjaan-jasa/status/", pekerjaan_jasa_status, name="pekerjaan_jasa_status"),
 ]
--- transaksi/urls.py
+++ transaksi/urls.py
@@ -5,6 +5,6 @@ from transaksi.views import mypay, mypay_transaksi
 app_name = "transaksi"
 
 urlpatterns = [
-    path("mypay", mypay, name="mypay"),
-    path("mypay/transaksi", mypay_transaksi, name="mypay_transaksi"),
+    path("mypay/", mypay, name="mypay"),
+    path("mypay/transaksi/", mypay_transaksi, name="mypay_transaksi"),
 ]
