50819306c641bc6be11c91e7ca0f374b9910b5a1
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2024-12-09 23:27:47 +0700

    Modify beli voucher

--- Diskon/views.py
+++ Diskon/views.py
@@ -9,6 +9,8 @@ from datetime import datetime, timedelta
 
 # Create your views here.
 def diskon(request):
+    idUser = request.session.get('user_id')
+    saldo = db.query_one("SELECT saldomypay FROM pengguna WHERE id = %s", (idUser,))['saldomypay']
     voucher_data = db.query_all("SELECT * FROM voucher JOIN diskon ON voucher.kode = diskon.kode")
     promo_data = db.query_all("SELECT * FROM promo JOIN diskon ON promo.kode = diskon.kode")
     metode_bayar = db.query_all("SELECT * FROM metode_bayar")
@@ -16,8 +18,16 @@ def diskon(request):
     context = {
         'vouchers': voucher_data,
         'promos': promo_data,
-        'metode_bayar': metode_bayar
+        'metode_bayar': metode_bayar,
+        'saldo': saldo,
     }
     
     return render(request, 'diskon.html', context)
 
+def beli_voucher(request, kode, idmetode, tglakhir):
+    tglawal = datetime.now()
+    idUser = request.session.get('user_id')
+    price = db.query_one("SELECT harga FROM voucher WHERE kode = %s", (kode,))['harga']
+    db.query_one("UPDATE pengguna SET saldomypay = saldomypay - %s WHERE id = %s", (price, idUser))
+    db.query_one("INSERT INTO TR_PEMBELIAN_VOUCHER VALUES (uuid_generate_v4(), %s, %s, 0, (SELECT Id from PELANGGAN WHERE Id = %s), %s, (SELECT Id from METODE_BAYAR WHERE NAMA = %s))", (tglawal, tglakhir, idUser, kode, idmetode))
+    return redirect('diskon')
