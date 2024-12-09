a528abbf7f6f1cc1570e38a0fd02dfab81e7aef4
Wisnu Nugroho <wisnunugroho614@gmail.com>
2024-12-09 20:03:50 +0700

    IMMIDIATE FIXXX: Locale problem

---
+++ authentication/templatetags/custom_filters.py
@@ -0,0 +1,18 @@
+from django import template
+import locale
+
+try:
+    locale.setlocale(locale.LC_ALL, "id_ID.UTF-8")
+except locale.Error:
+    locale.setlocale(locale.LC_ALL, "C.UTF-8")
+
+register = template.Library()
+
+
+@register.filter
+def format_idr(value):
+    try:
+        value = float(value)
+        return locale.currency(value, grouping=True, symbol="Rp ")
+    except (ValueError, TypeError):
+        return value
